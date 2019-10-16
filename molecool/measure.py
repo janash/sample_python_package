7aeb443e61a77614c7ff127bf70a6938c072bc6d
Jessica Nash <janash@vt.edu>
2019-10-16 12:49:04 -0400

    update measure module

--- molecool/measure.py
+++ molecool/measure.py
@@ -2,33 +2,48 @@
 molecool.py
 A Python package for analyzingnd visualizing xyz files. For MolSSI Workshop.
 
-Handles the primary functions
+This module is for functions which perform measurements.
 """
 
+import numpy as np
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
+def calculate_distance(rA, rB):
+    """Calculate the distance between two points.
 
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
+    rA, rB : np.ndarray
+        The coordinates of each point.
+    
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+    """
+    dist_vec = (rA - rB)
+    distance = np.linalg.norm(dist_vec)
+    return distance
+
+def calculate_angle(rA, rB, rC, degrees=False):
+    """Calculate the angle between three points.
+
+    Parameters
+    ----------
+    rA, rB, rC : np.ndarray
+        The coordinates of each point
+    degrees : bool , optional
+        Return unit of the calculated answer. Default is in radians.
 
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution
+    angle : float
+        The angle measurement for the three points.
     """
+    AB = rB - rA
+    BC = rB - rC
+    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    if degrees:
+        return np.degrees(theta)
+    else:
+        return theta
